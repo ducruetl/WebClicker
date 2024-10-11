96a910039db11d65379923738897ec61b718b4d7
ducruetl <leo.ducruet@etu.univ-grenoble-alpes.fr>
2024-10-11 19:08:05 +0200

    Added target animation for both mouse click and key pressed

--- javascript/clicker.js
+++ javascript/clicker.js
@@ -1,11 +1,12 @@
 let clickCounter = 0;
 let key = false;
+var id = null;
+
 document.getElementById("clickCounter").innerHTML=clickCounter;
 
 document.onkeydown = function () {
     if (!key) {
         onTargetClick();
-        document.getElementById("target").focus;
         key = true;
     }
 }
@@ -17,4 +18,33 @@ document.onkeyup = function () {
 function onTargetClick() {
     clickCounter++;
     document.getElementById("clickCounter").innerHTML=clickCounter;
+    animation();
+}
+
+function animation() {
+    var elem = document.getElementById("target");
+    var size = 250;
+    clearInterval(id);
+    id = setInterval(frame, 1);
+    
+    function frame() {
+        if (size == 200) {
+            clearInterval(id);
+            id = setInterval(frame2, 1);
+        } else {
+            size--;
+            elem.style.height = size + 'px';
+            elem.style.width = size + 'px';
+        }
+    }
+
+    function frame2() {
+        if (size == 250) {
+            clearInterval(id);
+        } else {
+            size++;
+            elem.style.height = size + 'px';
+            elem.style.width = size + 'px';
+        }
+    }
 }
