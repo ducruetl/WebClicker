b036bcf0d485153462042ec3498febc1c000ccc4
ducruetl <leo.ducruet@tu.univ-grenoble-alpes.fr>
2024-10-12 01:54:00 +0200

    Added menu to select differents target, sound when hitting target and button to enable/disable sound

--- javascript/clicker.js
+++ javascript/clicker.js
@@ -1,6 +1,8 @@
 let clickCounter = 0;
 let key = false;
 var id = null;
+var speaker = true;
+var hitSound = new Audio("audio/hit.mp3");
 
 document.getElementById("clickCounter").innerHTML=clickCounter;
 
@@ -18,7 +20,30 @@ document.onkeyup = function () {
 function onTargetClick() {
     clickCounter++;
     document.getElementById("clickCounter").innerHTML=clickCounter;
+    hitSound.currentTime = 0;
+    hitSound.play();
     animation();
+}
+
+function settings() {
+    document.getElementById("settings-menu").style.display = "flex";
+}
+
+function selectTarget(event) {
+    document.getElementById("target").src = event.currentTarget.src;
+    document.getElementById("settings-menu").style.display = "none";
+}
+
+function sound() {
+    if (speaker) {
+        speaker = false;
+        document.getElementById("sound").src = "images/speaker_off.png";
+        hitSound.volume = 0.0;
+    } else {
+        speaker = true;
+        document.getElementById("sound").src = "images/speaker_on.png";
+        hitSound.volume = 1.0;
+    }
 }
 
 function animation() {
@@ -28,7 +53,7 @@ function animation() {
     id = setInterval(frame, 1);
     
     function frame() {
-        if (size == 200) {
+        if (size == 240) {
             clearInterval(id);
             id = setInterval(frame2, 1);
         } else {
