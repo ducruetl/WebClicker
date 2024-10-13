21ba65740e16bc70c2792ba9399d1faf7663bddb
ducruetl <leo.ducruet@etu.univ-grenoble-alpes.fr>
2024-10-13 14:33:03 +0200

    Added nolan target to target menu

--- javascript/clicker.js
+++ javascript/clicker.js
@@ -17,9 +17,11 @@ document.onkeyup = function () {
     key = false;
 }
 
+
 function onTargetClick() {
     clickCounter++;
     document.getElementById("clickCounter").innerHTML=clickCounter;
+    hitSound.pause();
     hitSound.currentTime = 0;
     hitSound.play();
     animation();
